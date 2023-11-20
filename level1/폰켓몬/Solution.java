756f194384659039ba28c662e41a51ce8ba45c6c
jinseoplee <jjjp9966@gmail.com>
2023-11-20 18:04:43 +0900

    포켓몬 문제 추가

---
+++ level1/폰켓몬/Solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+	public int solution(int[] nums) {
+		Set<Integer> set = new HashSet<>();
+		for (int n : nums) {
+			set.add(n);
+		}
+
+		if (set.size() > nums.length / 2) {
+			return nums.length / 2;
+		}
+		return set.size();
+	}
+}
